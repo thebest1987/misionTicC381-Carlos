812e4a4c30883a7b3bc6978a6341adf0190ae27f
thebest1987 <carlos.higueram@hotmail.es>
2022-09-16 19:04:06 -0500

    servicios

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioServicios.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioServicios.cs
@@ -2,54 +2,65 @@ using System.Collections.Generic;
 using ProyectoCiclo3.App.Dominio;
 using System.Linq;
 using System;
+using Microsoft.EntityFrameworkCore;
  
 namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
 {
-    public class RepositorioUsuarios
+    public class RepositorioServicios
     { 
         
         private readonly AppContext _appContext = new AppContext();   
-
-        public IEnumerable<Usuario> GetAll()
+ 
+        public IEnumerable<Servicio> GetAll()
         {
-           return _appContext.Usuarios;
+           return _appContext.Servicios.Include(u => u.origen)
+                                    .Include(u => u.destino)
+                                    .Include(e => e.encomienda);
         }
  
-        public Usuario GetWithId(int id){
-            return _appContext.Usuarios.Find(id);
+        public Servicio GetWithId(int id){
+            return _appContext.Servicios.Find(id);
         }
-
-        // public Usuario Update(Usuario newUsuario){
-        //     var encomienda = _appContext.Usuarios.Find(newUsuario.id);
-        //     if(encomienda != null){
-        //         encomienda.descripcion = newUsuario.descripcion;
-        //         encomienda.peso = newUsuario.peso;
-        //         encomienda.tipo = newUsuario.tipo;
-        //         encomienda.presentacion = newUsuario.presentacion;
-        //         //Guardar en base de datos
-        //          _appContext.SaveChanges();
-        //     }
-        // return encomienda;
-        // }
-
-        // public Usuario Create(Usuario newUsuario)
-        // {
-        //    var addUsuario = _appContext.Usuarios.Add(newUsuario);
-        //     //Guardar en base de datos
-        //     _appContext.SaveChanges();
-        //     return addUsuario.Entity;
-        // }
-
-        // public Usuario Delete(int id)
-        // {
-        //     var encomienda = _appContext.Usuarios.Find(id);
-        // if (encomienda != null){
-        //     _appContext.Usuarios.Remove(encomienda);
-        //     //Guardar en base de datos
-        //     _appContext.SaveChanges();
-        // }
-        //  return null;  
-        // }
-
+ 
+        public Servicio Update(Servicio newServicio){
+            var encomienda = _appContext.Servicios.Find(newServicio.id);
+            if(encomienda != null){
+                encomienda.origen = newServicio.origen;
+                encomienda.destino = newServicio.destino;
+                encomienda.fecha = newServicio.fecha;
+                encomienda.hora = newServicio.hora;
+                encomienda.encomienda = newServicio.encomienda;
+                //Guardar en base de datos
+                 _appContext.SaveChanges();
+            }
+        return encomienda;
+        }
+ 
+        public Servicio Create(int origen, int destino, string fecha, string hora, int encomienda)
+        {
+            var newServicio = new Servicio();
+            newServicio.destino = _appContext.Usuarios.Find(destino);
+            newServicio.origen = _appContext.Usuarios.Find(origen);  
+            newServicio.encomienda = _appContext.Encomiendas.Find(encomienda);         
+            newServicio.fecha = DateTime.Parse(fecha);
+            newServicio.hora = hora;
+ 
+           var addServicio = _appContext.Servicios.Add(newServicio);
+            //Guardar en base de datos
+            _appContext.SaveChanges();
+            return addServicio.Entity;
+        }
+ 
+        public Servicio Delete(int id)
+        {
+            var encomienda = _appContext.Servicios.Find(id);
+        if (encomienda != null){
+            _appContext.Servicios.Remove(encomienda);
+            //Guardar en base de datos
+            _appContext.SaveChanges();
+        }
+         return null;  
+        }
+ 
     }
 }
